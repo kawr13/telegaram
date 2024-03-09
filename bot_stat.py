f41fcdc63cb0a803c32861fea46e5325a9cd3feb
kawr13 <62603910+kawr13@users.noreply.github.com>
2024-03-09 16:10:43 +0500

    Update bot_stat.py
    добавление докстринг

--- bot_stat.py
+++ bot_stat.py
@@ -1,6 +1,4 @@
 # телеграм_бот.py
-from ast import For
-from calendar import c
 from aiogram import Bot, Dispatcher, types, F
 import asyncio
 import io
@@ -21,63 +19,61 @@ from datetime import datetime
 import os
 from random import randint
 
-
+# Инициализация маршрутизатора для обработки сообщений бота
 router = Router()
 
-
+# Обработчик сообщения с текстом "Проверка заявок по контейнеру"
 @router.message(F.text == 'Проверка заявок по контейнеру')
-async def data_status_conteiner(messege: Message, state: FSMContext):
+async def data_status_conteiner(message: Message, state: FSMContext):
+    # Установка состояния FSMContext в Stat.conteiner
     await state.set_state(Stat.conteiner)
-    await messege.reply('Введите номер контейнера', reply_markup=ReplyKeyboardRemove())
-    
+    # Отправка сообщения с запросом на ввод номера контейнера и удаление клавиатуры
+    await message.reply('Введите номер контейнера', reply_markup=ReplyKeyboardRemove())
+
+# Обработчик сообщений в состоянии Stat.conteiner
 @router.message(Stat.conteiner, F.text)
 async def detecting(message: Message, state: FSMContext):
-    option = False
-    math = await cont_read(message.text)
-    if message.text == 'Завершить': 
-        messages = 'Выберите действие'
-        await message.reply(message.text, reply_markup=main_kb)
+    # Проверка на сообщение "Завершить"
+    if message.text == 'Завершить':
+        # Отправка сообщения с выбором действий и очистка состояния
+        await message.reply('Выберите действие', reply_markup=main_kb)
         await state.clear()
-    elif math is not None or coincidence(message.text):
-        if math and coincidence(math[0]) and len(math[0]) == 11:
-            for cont_num in math:
-                data = await data_async_cont(cont_num)
-                response_message = f'Контейнер: {data["result"][0]["ContNum"]}\n'
-                for dat in data.get("result"):
-                    if dat['Status'] is False:
-                        response_message += (
-                            # f"Срок действия:\n"
-                            # f"{formatted_date_from} - {formatted_date_until}\n"  
-                            f"Заявок не найдено\n"
-                            # f"Статус: {dat['Status']}"
-                        )
-                        continue
-                    else:
-                        date_object_until = datetime.strptime(dat["Until"], "%Y-%m-%dT%H:%M:%S")
-                        date_object = datetime.strptime(dat["From"], "%Y-%m-%dT%H:%M:%S")
-                        formatted_date_from = date_object.strftime("%d.%m.%Y")
-                        formatted_date_until = date_object_until.strftime("%d.%m.%Y")
-                        response_message += (
-                            # f"Срок действия:\n"
-                            # f"{formatted_date_from} - {formatted_date_until}\n"  
-                            f"начало действия заявки: 00:00 {formatted_date_from}\n"
-                            f"окончание:   23:59 {formatted_date_until}\n"
-                            # f"Статус: {dat['Status']}"
-                        )
-
-                # if len(response_message.split()) == 3:
-                #     response_message += 'Нет заявок'
-                await message.answer(response_message, reply_markup=canceling_kb)
-    
+    else:
+        # Проверка номера контейнера и его статуса
+        math = await cont_read(message.text)
+        if math is not None or coincidence(message.text):
+            if math and coincidence(math[0]) and len(math[0]) == 11:
+                for cont_num in math:
+                    data = await data_async_cont(cont_num)
+                    response_message = f'Контейнер: {data["result"][0]["ContNum"]}\n'
+                    for dat in data.get("result"):
+                        if dat['Status'] is False:
+                            response_message += (
+                                "Заявок не найдено\n"
+                            )
+                            continue
+                        else:
+                            date_object_until = datetime.strptime(dat["Until"], "%Y-%m-%dT%H:%M:%S")
+                            date_object = datetime.strptime(dat["From"], "%Y-%m-%dT%H:%M:%S")
+                            formatted_date_from = date_object.strftime("%d.%m.%Y")
+                            formatted_date_until = date_object_until.strftime("%d.%m.%Y")
+                            response_message += (
+                                f"начало действия заявки: 00:00 {formatted_date_from}\n"
+                                f"окончание:   23:59 {formatted_date_until}\n"
+                            )
+                    await message.answer(response_message, reply_markup=canceling_kb)
+            else:
+                messages = 'Номер введен некорректно'
+                await message.reply(messages, reply_markup=canceling_kb)
+                await state.set_data()
         else:
             messages = 'Номер введен некорректно'
             await message.reply(messages, reply_markup=canceling_kb)
             await state.set_data()
 
-    
-
+# Обработчик сообщения с текстом "Завершить" в состоянии Stat.conteiner
 @router.message(Stat.conteiner, F.text == 'Завершить')
 async def handle_photo(message: types.Message, state: FSMContext):
-    # Обработка фото
+    # Отправка сообщения с выбором действий и очистка состояния
     await message.reply('Выберите действие', reply_markup=main_kb)
     await state.clear()
