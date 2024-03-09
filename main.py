daaedfd6efb04d92897aefbe2758bfd0ba978e51
kawr13 <62603910+kawr13@users.noreply.github.com>
2024-03-09 15:52:42 +0500

    Update main.py
    добавление докстринг

--- main.py
+++ main.py
@@ -12,18 +12,28 @@ import bot_start
 # import bot_photo_load
 import bot_stat
 
+# Инициализация бота с указанным токеном и режимом разбора HTML
 bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
 
+# Основная асинхронная функция для запуска бота
 async def main():
+    # Настройка уровня логирования
     logging.basicConfig(level=logging.INFO)
+    
+    # Инициализация диспетчера для обработки обновлений от Telegram
     dp = Dispatcher()
+    
+    # Подключение маршрутизаторов для обработки команд бота
     dp.include_router(bot_start.router)
     # dp.include_router(bot_logic.router)
     # dp.include_router(bot_photo_load.router)
     dp.include_router(bot_stat.router)
     
+    # Удаление веб-хука бота и запуск его в режиме опроса
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+# Точка входа в приложение
 if __name__ == "__main__":
+    # Запуск асинхронной функции main с использованием asyncio
     asyncio.run(main())
