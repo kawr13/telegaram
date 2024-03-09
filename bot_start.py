e7759bb75613ed4f2fd406faf84a8437aeac38df
kawr13 <62603910+kawr13@users.noreply.github.com>
2024-03-09 16:06:53 +0500

    Update bot_start.py
    добавление докстринг

--- bot_start.py
+++ bot_start.py
@@ -1,6 +1,4 @@
 # телеграм_бот.py
-from ast import For
-from calendar import c
 from aiogram import Bot, Dispatcher, types, F
 import asyncio
 import io
@@ -20,34 +18,33 @@ from form.forms import Form, Stat
 import os
 from random import randint
 
-
-
+# Инициализация маршрутизатора для обработки сообщений бота
 router = Router()
 
+# Обработчик команды /start
 @router.message(CommandStart())
-async def echo_message(message: Message):
-    # await init()  # Вызывайте функцию инициализации базы данных перед использованием моделей
-    # if not await get_user(message.from_user.id):
-    #     if message.from_user.username is None or message.from_user.first_name is None or message.from_user.last_name is None:
-    #         username = 'Не указано'
-    #         first_name = 'Не указано'
-    #         last_name = 'Не указано'
-    #     else:
-    #         username = message.from_user.username
-    #         first_name = message.from_user.first_name
-    #         last_name = message.from_user.last_name
-    #     await create_user(username=username, usertelegram_id=message.from_user.id, first_name=first_name, last_name=last_name, password='')
-    # user = await get_user(message.from_user.id)
+async def start_message(message: Message):
+    # Инициализация базы данных
+    await init()
+    # Проверка существования пользователя в базе данных
+    user = await get_user(message.from_user.id)
+    if not user:
+        # Если пользователь не существует, создаем его с необходимыми данными
+        username = message.from_user.username if message.from_user.username else 'Не указано'
+        first_name = message.from_user.first_name if message.from_user.first_name else 'Не указано'
+        last_name = message.from_user.last_name if message.from_user.last_name else 'Не указано'
+        await create_user(username=username, usertelegram_id=message.from_user.id, first_name=first_name, last_name=last_name, password='')
+    # Отправка сообщения с клавиатурой действий
     await message.reply(f'Выберете действие', reply_markup=main_kb)
 
+# Обработчик команды /help
 @router.message(Command(commands=['help']))
 async def process_helper(message: Message):
+    # Отправка сообщения с инструкциями по началу работы
     await message.reply('Напишите /start для начала работы')
-    
 
+# Пример обработчика, который не используется в вашем коде
 # @router.message(F.text != 'Проверка заявок по контейнеру')
 # async def canceling(message: Message):
 #     messages = 'Выберите действие'
 #     await message.reply(messages, reply_markup=main_kb)
-
-    
